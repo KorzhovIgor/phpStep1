88ee4960e8ddfa28848c248c8fd597ca84a145b4
Igor Korzhov <igor.korzhov@innowise-group.com>
2023-05-05 08:28:24 +0200

    C-2-3| reworked create and show views

--- app/views/Comments/create.php
+++ app/views/Comments/create.php
@@ -10,7 +10,8 @@
 <nav class="navbar navbar-expand-lg bg-light">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
@@ -24,38 +25,19 @@
 </nav>
 <div class="container">
     <div class="card p-5 mt-5">
-        <form>
+        <form name="commentsForm" method="post" action="/comments/store" id="ddd">
             <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                <label for="title" class="form-label">Title:</label>
+                <input type="text" name="title" class="form-control" id="title" aria-describedby="emailHelp" required>
             </div>
-            <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
-            </div>
-            <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            <div class="form-floating mb-3">
+                <textarea name="content" class="form-control" placeholder="Leave a comment here"
+                          id="floatingTextarea" required></textarea>
+                <label for="floatingTextarea">Content</label>
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
     </div>
-
-
-    <form name="commentsForm" method="post" enctype="multipart/form-data" action="/comments/store" id="ddd">
-        <div>
-            <label for="title">Title:</label>
-        </div>
-        <input id="title" name="title" type="text" required/>
-        <div>
-            <label for="content">Content:</label>
-        </div>
-        <textarea name="content" id="content" cols="30" required rows="10"></textarea>
-        <div>
-            <button id="submitComment" type="submit">Submit</button>
-        </div>
-    </form>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
