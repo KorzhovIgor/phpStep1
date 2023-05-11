ee774c363b0d5f44cc5dfa2dcecb52e22b502588
Igor Korzhov <igor.korzhov@innowise-group.com>
2023-05-11 08:55:28 +0200

    C-2-3-1| reworked index.php

--- app/views/Comments/index.php
+++ app/views/Comments/index.php
@@ -34,9 +34,11 @@
             <div class="col m-2">
                 <div class="card">
                     <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <h5 class="comment-id"> <?= $allComments[$i]->id . ' ' . $allComments[$i]->title ?></h5>
+                        <p class="card-text"><?= $allComments[$i]->content ?></p>
+                        <a class="btn btn-primary" href="/comment/<?= $allComments[$i]->id?>">Open</a>
+                        <button id="deleteButton" class="btn btn-primary" data-id="<?= $allComments[$i]->id ?>"
+                                type="button">Delete</button>
                     </div>
                 </div>
             </div>
@@ -53,121 +55,6 @@
         <?php endfor; ?>
     </ul>
 </nav>
-
-
-
-
-
-
-
-
-
-
-    <!--<div class="mt-5 mb-2">
-        <h1>Comments page</h1>
-    </div>
-    <div class="d-flex justify-content-between flex-lg-row flex-md-column">
-        <div class="col m-2">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-        <div class="col m-2">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-        <div class="col m-2">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="d-flex justify-content-between flex-lg-row flex-md-column">
-        <div class="col m-2">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-        <div class="col m-2">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-        <div class="col m-2">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-    </div>
-</div> -->
-    <!--<div class="card">
-        <table class="table table-hover">
-            <thead>
-            <tr>
-                <th scope="col">id</th>
-                <th scope="col">title</th>
-                <th scope="col">content</th>
-                <th scope="col">created at</th>
-                <th scope="col">#</th>
-            </tr>
-            </thead>
-            <tbody>
-            <?php foreach($allComments as $comment): ?>
-                <tr>
-                    <td>
-                       <?= $comment->id ?>
-                    </td>
-                    <td>
-                        <?= $comment->title ?>
-                    </td>
-                    <td class ="text-break">
-                        <?= $comment->content ?>
-                    </td>
-                    <td>
-                        <?= $comment->created_at ?>
-                    </td>
-                    <td>
-                        <a class="btn btn-primary" href="/comment/<?= $comment->id?>">Open</a>
-                        <button id="deleteButton" class="btn btn-primary" data-id="<?= $comment->id ?>"
-                                type="button">Delete</button>
-                    </td>
-                </tr>
-            <?php endforeach; ?>
-            </tbody>
-        </table>
-        <nav aria-label="Page navigation example">
-            <ul class="pagination justify-content-center">
-                <?php for($i = 1; $i <= $countPages; $i++): ?>
-                    <li class="page-item"><a class="page-link" href="/comments?page=<?= $i ?>"><?=$i ?></a></li>
-                <?php endfor; ?>
-            </ul>
-        </nav>
-    </div> -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 <script src="/assets/javascript/confirmDelete.js"></script>
