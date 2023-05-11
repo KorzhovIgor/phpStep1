ae5e8fa29d7f8a0940f6773d887a60d7b9f4805e
Igor Korzhov <igor.korzhov@innowise-group.com>
2023-05-11 10:56:54 +0200

    C-2-3-1| was added mobile css properties

--- app/views/Comments/index.php
+++ app/views/Comments/index.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=yes">
     <title>MyApp</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
@@ -29,7 +30,7 @@
     </div>
     <?php for ($i = 0, $j = 0; $i < count($allComments); $i++, $j++): ?>
         <?php if($j == 0): ?>
-            <div class="d-flex justify-content-between flex-lg-row flex-md-column">
+            <div class="d-flex justify-content-between flex-lg-row flex-column">
         <?php endif; ?>
             <div class="col m-2">
                 <div class="card">
